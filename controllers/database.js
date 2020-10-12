bc0dda1782fede9f7a2ee26e799fcd07e72e5cf5
Georgi <joreto50@gmail.com>
2020-10-12 18:09:18 +0300

    retrive cubes from json file and render them to main and details pages

---
+++ controllers/database.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+const filePath = path.resolve(__dirname, "../config/database.json");
+
+function saveCube(CubeData){
+    getCubes((db)=>{
+        db.push(CubeData);
+        fs.writeFile(filePath, JSON.stringify(db), (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(
+                ` ${CubeData.id} is succesfully stored in ${filePath} `
+              );
+            }
+          });
+   });
+    
+}
+
+function getCube(id,callback){
+   getCubes((cubes)=>{
+        const match = cubes.filter(c => c.id === id);
+        callback(match[0]);
+   })
+}
+
+function getCubes(callback){
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const db = JSON.parse(data);
+          callback(db);
+        }
+      });
+}
+
+
+module.exports = { 
+    saveCube,
+    getCube,
+    getCubes
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 // TODO: Require Controllers...
 
 const { Router } = require("express");
+const { getCubes, getCube } = require("../controllers/database");
 
 const router = new Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Cube Workshop" });
+  getCubes((cubes)=>{
+    res.render("index", { title: "Cube Workshop", cubes });
+  })
 });
 
 router.get("/about", (req, res) => {
@@ -16,8 +19,11 @@ router.get("/create", (req, res) => {
   res.render("create", { title: "Cube Workshop|Create" });
 });
 
-router.get("/details:id", (req, res) => {
-  res.render("details", { title: "Cube Workshop|Details" });
+router.get("/details/:id", (req, res) => {
+  getCube(req.params.id,(cube)=>{
+    res.render("details", { title: "Cube Workshop|Details" , ...cube });
+  })
+  
 });
 
 router.get("*", (req, res) => {
