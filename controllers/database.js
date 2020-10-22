ed8985321b217f8ba7a30b077d689ee63b6cf563
Georgi <joreto50@gmail.com>
2020-10-23 00:30:41 +0300

    replaced saving and retrieving cubes from db to atlas/created new mongo cube models

--- controllers/database.js
+++ controllers/database.js
@@ -1,20 +1,6 @@
 const mongoose = require("mongoose");
 const Cube = require("../models/cube");
 
-function saveCube(CubeData, callback) {
-  getCubes((db) => {
-    db.push(CubeData);
-    fs.writeFile(filePath, JSON.stringify(db), (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(` ${CubeData.id} is succesfully stored in ${filePath} `);
-        callback();
-      }
-    });
-  });
-}
-
 async function getCube(id) {
   const cube = await Cube.findById(id).lean();
   return cube;
@@ -23,26 +9,6 @@ async function getCube(id) {
 async function getCubes() {
   const cubes = await Cube.find().lean();
   return cubes;
-}
-
-function getSearch(callback, search, to, from) {
-  to = to.length === 0 ? 7 : Number(to);
-  from = from.length === 0 ? 1 : Number(from);
-
-  getCubes((cubes) => {
-    let matches = [];
-
-    cubes.forEach((element) => {
-      if (
-        element.name === search ||
-        (element.dificultyLevel >= from && element.dificultyLevel <= to)
-      ) {
-        matches.push(element);
-      }
-    });
-
-    callback(matches);
-  });
 }
 
 module.exports = {
